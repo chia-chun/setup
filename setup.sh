c195fc73f7024e5cf8670c9ff42507158e3f7e0a
chia-chun <chia-chun@users.noreply.github.com>
2014-05-21 11:17:10 +0800

    some comments

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-cd ~
+# 1. install git and git clone https://github.com/chia-chun/setup ~/setup
+# 2. visudo to put current user in the sudoer list
+# 3. make sure that /etc/apt/sources.list is up-to-date and targeting testing
+sudo apt-get update
+sudo apt-get dist-upgrade -y
 sudo apt-get install -y emacs24 #"emacs" would install emacs 23 on 2014-5-19
 sudo apt-get install -y guile-2.0
 git config --global user.name "chia-chun"
 git config --global user.email chia-chun@users.noreply.github.com
-git clone https://github.com/chia-chun/.emacs.d
+git clone https://github.com/chia-chun/.emacs.d ~/.emacs.d
+# The .Xmodmap swaps Ctrl and Alt, CapsLock and Escape.
 cp ~/setup/.Xmodmap ~/
+# The .xinitrc makes .Xmodmap working when starting X.
 cp ~/setup/.xinitrc ~/
