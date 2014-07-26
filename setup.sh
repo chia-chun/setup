2c8b8ebecf0de827e4330d560abe5c75f008aac6
chia-chun <chia-chun@users.noreply.github.com>
2014-07-26 09:49:13 +0800

    guile-dev

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install -y emacs24 #"emacs" would install emacs 23 on 2014-5-19
 sudo apt-get install -y guile-2.0
+sudo apt-get install -y guile-2.0-dev
 sudo apt-get install -y guile-cairo
 sudo apt-get install -y r-base
 sudo apt-get install -y emacs-goodies-el
