4284e7da8fc73e7799e4fc49b914480b103cbd95
chia-chun <chia-chun@users.noreply.github.com>
2015-04-06 15:50:56 +0800

    biglm

--- init.R
+++ init.R
@@ -1,3 +1,3 @@
 options("repos" = c(CRAN = "http://cran.rstudio.com"))
 update.packages(checkBuilt=TRUE, ask=FALSE)
-install.packages(c("ggplot2","ff","ffbase","gridExtra","fdrtool"))
+install.packages(c("ggplot2","ff","ffbase","gridExtra","fdrtool","biglm"))
